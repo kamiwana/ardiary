0448e22b53595343b0a98239a40ccecb48d28976
kamiwana <kamiwanan@gmail.com>
2020-02-16 17:12:15 +0900

    .

--- ardiary/urls.py
+++ ardiary/urls.py
@@ -18,6 +18,22 @@ from django.urls import path, include # new
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ARDiary API",
+      default_version='v1',
+      description="ARDiary description",
+   ),
+   validators=['flex'], #'ssv'],
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 admin.site.site_title = "ARDiary"
 admin.site.site_header = "ARDiary"
@@ -26,9 +42,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^auth/', include('users.urls')),
     url(r'^contents/', include('contents.urls')),
-    #path('accounts/', include('allauth.urls')),
-    #path('rest-auth/', include('rest_auth.urls')),
-    #path('rest-auth/registration/', include('rest_auth.registration.urls'))
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/v1$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+ #   path('accounts/', include('allauth.urls')),
+#    path('rest-auth/', include('rest_auth.urls')),
+#    path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
 
 if settings.DEBUG:
